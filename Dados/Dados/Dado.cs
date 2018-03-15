a5bb5134ded22631225d9ea5a86960dbd65f4736
saloewe <31968717+saloewe@users.noreply.github.com>
2018-03-15 08:59:06 -0600

    Add files via upload

---
+++ Dados/Dados/Dado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dados
+{
+    class Dado
+    {
+        Random rn = new Random();
+       
+        public int Lanzar()
+        {
+            int tiro = rn.Next(1, 7);
+            return tiro;
+        }
+    }
+}
---
+++ Dados/Dados/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dados
+{
+    public partial class Form1 : Form
+    {   Dado d;
+        
+        public Form1()
+        { 
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text += Convert.ToString(d.Lanzar()) + "\r\n" ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] vector = new int[6];
+            for (int i = 0; i < 100; i++)
+            {
+                int c = d.Lanzar();
+                vector[c - 1]++;
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                textBox2.Text += Convert.ToString(vector[i]) +"\r\n" ; 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] vector = new int[12];
+            for (int i = 0; i < 100; i++)
+            {
+                int l1 = d.Lanzar();
+                int l2 = d.Lanzar();
+                int c = l1 + l2;
+                vector[c - 1]++;
+            }
+            for (int i = 1; i < 12; i++)
+            {
+                textBox3.Text += Convert.ToString(vector[i]) + "\r\n";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+             d = new Dado();
+            
+        }
+
+        
+    }
+}
